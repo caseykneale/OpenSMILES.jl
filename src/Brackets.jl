bb5aa0e00bcb76d19773c8e1fea4480ecc2fc54e
Tim Holy <tim.holy@gmail.com>
2020-11-13 07:57:02 -0600

    Improve Bracket parsing and full chirality parsing
    This also corrects empirical-formula generation to avoid printing "H0"
    when there are no hydrogens.

--- src/Brackets.jl
+++ src/Brackets.jl
@@ -7,76 +7,71 @@
 end
 
 struct BracketParseException <: Exception
+    msg::String
     bracketcontents::String
-    charloc::Int64
+    charloc::Int
 end
-Base.showerror(io::IO, e::BracketParseException) = print(io, "Failed to parse bracket(\"[$(e.bracketcontents)]\") on character ", e.charloc, "!")
+Base.showerror(io::IO, e::BracketParseException) = print(io, "Failed to parse bracket(\"[$(e.bracketcontents)]\") on character ", e.charloc, "!\n", e.msg)
 
-function ParseBracket(S)
-    Sorig = S
-    origlen = curlen = lastlen = length(S)
+function ParseBracket(s::AbstractString)
+    len = length(s)
 
     state = ISOTOPE
     symbol, isotope, aromatic, ringID, hydrogens, charge = nothing, nothing, false, [], 0, 0
 
-    while curlen > 0
-        cursor = S[1]
+    idx = 1
+    while idx <= len
+        cursor = s[idx]
         #Handle isotope, explicit hydrogen multiplicity
-        if isdigit( cursor )
+        if isdigit(cursor)
             if state == ISOTOPE
-                isotope, S = ReadNextNumeric( S )
+                isotope, idx = tryparseint16(s, idx)
                 state = SYMBOL
             elseif state == MULTIPLICITY
-                hydrogens, S = ReadNextNumeric( S )
+                hydrogens, idx = tryparseint16(s, idx)
                 state = CHARGE
             else
-                @warn("Invalid SMILES. Charge must be denoted with +/-.")
+               throw(BracketParseException("Invalid SMILES. Charge must be denoted with +/-.", s, idx))
             end
         #Handle Element symbol parsing
-        elseif isletter( cursor )
-            if ( state == SYMBOL ) || (state == ISOTOPE)
-                if isa( symbol, Nothing )
-                    symbollist = isuppercase( cursor ) ? bracket : bracket_aromatic
-                    aromatic = islowercase( cursor )
-                    symbol, S = ReadNextElement( S, symbollist )
+        elseif isletter(cursor)
+            if state == SYMBOL || state == ISOTOPE
+                if isa(symbol, Nothing)
+                    symbollist = isuppercase(cursor) ? bracket : bracket_aromatic
+                    aromatic = islowercase(cursor)
+                    symbol, idx = tryparseelement(s, idx, symbollist)
                     if isa(symbol, Nothing)
-                        @warn("Invalid SMILES. Unrecognized chemical symbol.")
+                        throw(BracketParseException("Invalid SMILES. Unrecognized chemical symbol.", s, idx))
                     end
-                elseif S[1] == 'H'
+                elseif cursor == 'H'
                     state = MULTIPLICITY
                     hydrogens = 1
-                    S = S[2:end]
+                    idx = nextind(s, idx)
                 else
-                    @warn("Invalid SMILES. Bracket should contain one element (with or without Hydrogen).")
+                    throw(BracketParseException("Invalid SMILES. Bracket should contain one element (with or without Hydrogen).", s, idx))
                 end
             else
-                @warn("Invalid SMILES. Bracket should contain one element.")
+                throw(BracketParseException("Invalid SMILES. Bracket should contain one element.", s, idx))
             end
         #Handle charge
         elseif ispm(cursor)
-            if ( state == CHARGE ) || ( state == MULTIPLICITY ) || ( state == SYMBOL ) || ( state == ISOTOPE )
-                charge, S = ReadNextCharge( S )
+            if state == CHARGE || state == MULTIPLICITY || state == SYMBOL || state == ISOTOPE
+                charge, idx = tryparsecharge(s, idx)
                 state = SYMBOL
             else
-                @warn("Invalid SMILES. Charge should follow element symbol.")
+                throw(BracketParseException("Invalid SMILES. Charge should follow element symbol.", s, idx))
             end
         #Handle operators
         elseif isspecialoperator(cursor)
-            @warn("Invalid SMILES. Special operators(" * join(specialoperators, ",") * ") should not be contained in a bracket.")
+            throw(BracketParseException("Invalid SMILES. Special operators (" * join(specialoperators, ",") * ") should not be contained in a bracket.", s, idx))
         elseif isbondoperator(cursor)
             if cursor == '@'
                 @warn("Chirality is not yet supported, ignoring @ operator", maxlog=1)
-                S = S[2:end]
+                _, idx = tryparsechirality(s, idx)
             else
-                @warn("Invalid SMILES. Bond operators(" * join(bondoperators, ",") * ") should not be contained in a bracket.")
+                throw(BracketParseException("Invalid SMILES. Bond operators(" * join(setdiff(bondoperators, '@'), ",") * ") should not be contained in a bracket.", s, idx))
             end
         end
-        #Ensure we don't get stuck in an infinite loop
-        curlen = length( S )
-        if lastlen == curlen
-            throw(BracketParseException( Sorig, (origlen - curlen + 1) ))
-        end
-        lastlen = curlen
     end
     return Element( symbol, isotope, aromatic, ringID, hydrogens, 0, charge )
 end
--- src/ReadFns.jl
+++ src/ReadFns.jl
@@ -25,6 +25,24 @@ function ReadNextElement( S::String , List::Array{ String, 1 } )
     return NextElement, S
 end
 
+function tryparseelement(s::AbstractString, idx::Int, elements)
+    len = length(s)
+    idx > len && return nothing, idx
+    c = s[idx]
+    isletter(c) || return nothing, idx
+    idxnext = nextind(s, idx)
+    cnext = idxnext <= len ? s[idxnext] : '1'
+    if islowercase(cnext)
+        # Try a 2-letter element
+        element = c*cnext
+        element ∈ elements && return element, nextind(s, idxnext)
+    end
+    # Try a 1-letter element
+    element = string(c)
+    element ∈ elements && return element, idxnext
+    return nothing, idx
+end
+
 #Fn for parsing the inside of a bracket...
 function ReadNextNumeric(S::String)
     len = length(S)
@@ -43,6 +61,17 @@ function ReadNextNumeric(S::String)
     return isotope, S
 end
 
+function tryparseint16(s::AbstractString, idx::Int)
+    len = length(s)
+    lastidx = prevind(s, idx)
+    while lastidx < len
+        i = nextind(s, lastidx)
+        isdigit(s[i]) || break
+        lastidx = i
+    end
+    return lastidx >= idx ? (parse(Int16, SubString(s, idx, lastidx)), nextind(s, lastidx)) : (nothing, idx)
+end
+
 function ReadNextCharge(S::String)
     len = length(S)
     charge = nothing
@@ -59,4 +88,41 @@ function ReadNextCharge(S::String)
         end
     end
     return charge, ""
+end
+
+function tryparsecharge(s::AbstractString, idx::Int)
+    len = length(s)
+    idx > len && return nothing, idx
+    c = s[idx]
+    ispm(c) || return nothing, idx
+    sgn = c == '+' ? 1 : -1
+    idxnext = nextind(s, idx)
+    count = 1
+    while idxnext <= len
+        cnext = s[idxnext]
+        if cnext == c
+            count += 1
+            @warn("++ and -- are deprecated in the OpenSMILES specification, please use + or - followed by multiplicity.", maxlog=1)
+            idxnext = nextind(s, idxnext)
+        elseif ispm(cnext)
+            error("+- and -+ are not allowed for charge in SMILES")
+        elseif isdigit(cnext)
+            count == 1 || error("cannot use ++ or -- in charge specification followed by multiplicity")
+            break
+        else
+            return sgn*count, idxnext
+        end
+    end
+    idxnext > len && return sgn*count, idxnext
+    mult, idxnext = tryparseint16(s, idxnext)
+    mult === nothing && return sgn, idxnext
+    @assert count == 1
+    return sgn*mult, idxnext
+end
+
+function tryparsechirality(s::AbstractString, idx::Int)
+    m = match(r"(@TH[12]|@AL[12]|@SP[1-3]|@TB\d\d?|@OH\d\d?|@@?)", s, idx)
+    (m === nothing || m.offset > idx) && return nothing, idx
+    chirality = m.captures[1]::AbstractString
+    return chirality, m.offset + lastindex(chirality)
 end
